8e09fd5243289b22556bf39a2b6a253661bd70b4
devjindev <hyjinny00@gmail.com>
2023-01-12 13:58:26 +0900

    ♻️ 리팩토링#1: styles 폴더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/Auth";
-import "./App.css";
 import NotFound from "./pages/NotFound";
 import { useEffect } from "react";
 import TodoDetail from "./pages/TodoDetail";
 import { useSetRecoilState } from "recoil";
 import { loginState } from "./atom/auth";
+import axios from "axios";
 
 function App() {
   const setLogin = useSetRecoilState(loginState);
 
+  // 새로고침 할 때마다 적용
   useEffect(() => {
     if (localStorage.getItem("token")) {
+      axios.defaults.headers.Authorization = localStorage.getItem("token");
       setLogin(true);
     }
   }, [setLogin]);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import GlobalStyle from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <RecoilRoot>
+    <GlobalStyle />
     <App />
   </RecoilRoot>
 );
